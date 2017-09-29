4ae58a290737778695c4e9519166f74db6867e8c
gabarnettjr <gabarnettjr@gmail.com>
2017-09-29 13:03:43 -0600

    add bash script for compiling test of fraction class

---
+++ fraction/compileFractionTest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# echo $1 goes to the store and buys $2.
+
+# echo -e "Please enter your name: "
+# read name
+# echo "Nice to meet you $name."
+
+g++ frac$1.hpp frac$1.cpp ftest$1.cpp
