4206aa2cc4d2a1aa585fe965d3a5a228d421aede
Greg Barnett <gabarnettjr@gmail.com>
2017-09-30 01:55:27 -0600

    one more check

--- charSet/charSet.cpp
+++ charSet/charSet.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	//for ever character c with a value of 32 to 126:
+	//for every character c with a value of 32 to 126:
 	for ( unsigned char c=32; c<127; ++c )
 	{
 		//output value as a number and as a character:
