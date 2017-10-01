172b44ce63b65b48f7159f2e036c0b6434f635b2
Greg Barnett <gabarnettjr@gmail.com>
2017-10-01 11:50:51 -0600

    reads in txt file to get either vector or matrix

--- learningStrings/inputString.cpp
+++ learningStrings/inputString.cpp
@@ -1,24 +1,63 @@
+//Learning how to use information in the separate
+//file, in.txt, to create an array.
+//The first two numbers in in.txt give the size
+//of the matrix, and the rest of the numbers
+//give the entries in the matrix, so the first
+//two need to be ints and the remaining can
+//be doubles.
+
 #include <iostream>
 #include <fstream>
 
 int main()
 {
-	int m;
-	int n;
+	int d;
 	std::ifstream inFile;
 	inFile.open( "in.txt" );
-	inFile >> m;
-	inFile >> n;
-	std::cout << "m = " << m << std::endl;
-	std::cout << "n = " << n << std::endl;
-	double x[m*n];
-	for( int i=0; i<m*n; i++ )
+	inFile >> d;
+	if( d == 1 )
 	{
-		inFile >> x[i];
+		int m;
+		inFile >> m;
+		std::cout << "d = " << d << std::endl;
+		std::cout << "m = " << m << std::endl;
+		double x[m];
+		for( int i=0; i<m; i++ )
+		{
+			inFile >> x[i];
+		}
+		inFile.close();
+		for( int i=0; i<m; i++ )
+		{
+			std::cout << "x[" << i << "] = " << x[i] << std::endl;
+		}
 	}
-	inFile.close();
-	for( int i=0; i<m*n; i++ )
+	else if( d == 2 )
 	{
-		std::cout << "x[" << i << "] = " << x[i] << std::endl;
+		int m, n;
+		inFile >> m >> n;
+		std::cout << "d = " << d << std::endl;
+		std::cout << "m = " << m << std::endl;
+		std::cout << "n = " << n << std::endl;
+		double x[m*n];
+		for( int i=0; i<m*n; i++ )
+		{
+			inFile >> x[i];
+		}
+		inFile.close();
+		for( int i=0; i<m*n; i++ )
+		{
+			std::cout << "x[" << i << "] = " << x[i] << std::endl;
+		}
 	}
+	//double x[m*n];
+	//for( int i=0; i<m*n; i++ )
+	//{
+	//	inFile >> x[i];
+	//}
+	//inFile.close();
+	//for( int i=0; i<m*n; i++ )
+	//{
+	//	std::cout << "x[" << i << "] = " << x[i] << std::endl;
+	//}
 }
