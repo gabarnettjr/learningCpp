8fccb0a7a9c657d1bc7bae1f1e867150ee8b25a8
gabarne <gabarne@sandia.gov>
2017-10-16 17:05:20 -0600

    learning how to use vectors and vectors of vectors(matrices)

---
+++ learningCmath/usingStdVec.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+int main( int argc, char * argv[] )
+{
+	{
+    	double x = exp(-2);
+    	std::cout << "x = " << x << std::endl;
+	}
+	{
+		int x = 4;
+		std::cout << "x = " << x << std::endl;
+	}
+	{
+		int n = 10;
+		std::vector<double> x;
+		for( int i=0; i<n; i++ )
+		{
+			x.push_back( i*i );
+		}
+    	for( int i=0; i<n; i++ )
+		{
+    	    std::cout << "x[" <<  i << "] = " << x[i] << std::endl;
+    	}
+		{
+			std::vector<double> y;
+			std::vector<double> z;
+			for( int i=0; i<n; i++ )
+			{
+				y.push_back(i+M_PI);
+				z.push_back(i+cos(1));
+			}
+			std::vector< std::vector<double> > A;
+			A.push_back(x);
+			A.push_back(y);
+			A.push_back(z);
+			for( int i=0; i<3; i++ )
+			{
+				for( int j=0; j<n; j++ )
+				{
+					std::cout << "A[" << i << "][" << j << "] = " << A[i][j] << std::endl;
+				}
+			}
+		}
+	}
+	return 0;
+}
