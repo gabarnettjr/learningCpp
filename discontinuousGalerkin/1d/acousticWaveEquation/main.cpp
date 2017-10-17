0f3a6e7b8b31c0e0b6918b8b9ffcfbeeadc3aacf
gabarne <gabarne@sandia.gov>
2017-10-17 14:22:57 -0600

    initializing rho and u with X

--- discontinuousGalerkin/1d/acousticWaveEquation/main.cpp
+++ discontinuousGalerkin/1d/acousticWaveEquation/main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <cmath>
 
+//The initial condition for rho, which should be periodic:
+double rhoIC( double x )
+{
+    return exp(-10*x*x);
+}
+
 int main()
 {
 	double a = -1.;
 	double b = 1.;
 	int np = 4;
 	int ne = 4;
+    
+    int i, j, k;
 
 	//Element boundary points:
 	double dx = (b-a) / ne;
 	double x[ne+1];
-	for( int i=0; i<ne+1; i++ )
+	for( i=0; i<ne+1; i++ )
 	{
 		x[i] = a + i*dx;
 	}
@@ -42,25 +50,37 @@ int main()
 
 	//Location of the center of each element:
 	double xc[ne];
-	for( int i=0; i<ne; i++ )
+	for( i=0; i<ne; i++ )
 	{
 		xc[i] = ( x[i] + x[i+1] ) / 2;
 	}
 
-	//All of the x-coordinates in one long array:
+	//All of the x-coordinates in one long array.
+	//Also all of initial values of rho and u:
 	double X[N];
-	for( int i=0; i<ne; i++ )
+	double rho[N];
+	double u[N];
+	for( i=0; i<ne; i++ )
 	{
-		for( int j=0; j<np; j++ )
+		for( j=0; j<np; j++ )
 		{
 			X[np*i+j] = xc[i] + dx/2 * xGLL[j];
+			rho[np*i+j] = rhoIC( X[np*i+j] );
+			u[np*i+j] = 0.;
 		}
 	}
-	//Print X to make sure it is correct:
-	for( int i=0; i<N; i++ )
+    
+	//Print X and rho to make sure they are correct:
+    std::cout << std::endl;
+	for( i=0; i<N; i++ )
 	{
 		std::cout << "X[" << i << "] = " << X[i] << std::endl;
 	}
-	
-
+    std::cout << std::endl;
+    for( i=0; i<N; i++ )
+	{
+        std::cout << "rho[" << i << "] = " << rho[i] << std::endl;
+	}
+    
+    
 }
